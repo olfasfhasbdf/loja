31c9929e4dc4490850f3e9cabbd171fff46118ed
olfasfhasbdf <100292859+olfasfhasbdf@users.noreply.github.com>
2023-05-30 11:49:24 -0300

    Add files via upload

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+
+function Header(){
+    return(
+        <>
+        <div className="header">
+            <a href="/">Simas Turbo Peças</a>
+        </div>
+        </>
+    )
+}
+export default Header
